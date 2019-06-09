2cc019794c25e6ccf30dcf5aa68030f10fc0b4d9
Kataki8 <katakimlg@gmail.com>
2019-06-09 21:27:22 +0300

    Lesson1

---
+++ Lesson 1/Homework/4.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    a = [4,5,1,3,2]
+    a.sort()
+    print(a)
---
+++ Lesson 1/Homework/5.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+  print ('Enter a:')
+  a = float(input())
+  print ('Enter b:')
+  b = float(input())
+  c = a % (10)
+  d = b % (10)
+
+  print(c+d)
